c46e7bbe16522a15fdb53ee12a73eb4270ef38b5
ThomasTVV <48794323+ThomasTVV@users.noreply.github.com>
2022-09-25 10:31:33 +0200

    api ting færdig.

--- api/oasis.js
+++ api/oasis.js
@@ -3,23 +3,20 @@
 'use strict';
 const request = require('superagent');
 const fs = require('fs');
+const admZip = require('adm-zip');
 
 //Installation:
 // download node
-// I terminal: npm install
+// I terminal: cd api && npm install
 
 
 //Guide:          --start--      --slut--
-// downloadZips(2022, 09, 22, 2022, 09, 23); //dato i vores tid! - ikke sikkert det er samme dato california.
+// downloadZips(2022, 09, 22, 2022, 09, 23);
 //eller:
 downloadYesterday();
 
-//kør: cd api
-// node oasis
-
-// den har issues ved at downloade - end point er vidst rigtig tho.
-// TODO: UTC ting skal fixes. ELLER MÅSKE BARE SÆT TXX MANUELT.ß
-// Extract zip files i "csv" folder.
+//kør: cd api && node oasis.js
+// (den tager ca. 4 min pr. dag der skal hentes.)
 
 
 
@@ -27,7 +24,7 @@ async function downloadZip(startdatetime, groupId = 'RTPD_LMP_GRP') {
     //inspiration: https://digitaldrummerj.me/node-download-zip-and-extract/ 
     const baseUrl = 'http://oasis.caiso.com/oasisapi/GroupZip?version=1&resultformat=6';
     const urlWithParams = `${baseUrl}&groupid=${groupId}&startdatetime=${startdatetime}`;
-    console.log('urlWithParams:', urlWithParams);
+    console.log('downloader:', urlWithParams);
 
     const hourForFileName = startdatetime.split(':')[0].split('T')[1];
     const zipFileName = `./zip_files/${groupId}-T${hourForFileName}.zip`;
@@ -39,17 +36,19 @@ async function downloadZip(startdatetime, groupId = 'RTPD_LMP_GRP') {
     })
     .pipe(fs.createWriteStream(zipFileName))
     .on('finish', function() {
-        // add code below to here
+        var zip = new admZip(zipFileName);
+        zip.extractAllToAsync('./csv_files', true);
+        console.log('unzipped.');
     });
 
 }
 
-function formatDate(date, opr_hour) {
+function formatDate(date, hour) {
     const text = date.toISOString(); //altid zero utc time.
     const tmp1 = text.replaceAll('-', '');
     const tmp2 = tmp1.split(':')[0];
     const tmp3 = tmp2.substring(0, tmp2.length - 2);
-    const t = opr_hour < 10 ? `0${opr_hour}` : opr_hour;
+    const t = hour < 10 ? `0${hour}` : hour;
     const tmp4 = tmp3 + t;
     const tmp5 = tmp4 + ':00-0000';
     return tmp5;
@@ -58,10 +57,12 @@ function formatDate(date, opr_hour) {
 
 async function downloadZips(startYear, startMonth, startDay, endYear, endMonth, endDay) {
     let dateToDownload = new Date(startYear, startMonth - 1, startDay); //month er zero indexed.
+    dateToDownload.setTime(dateToDownload.getTime() + 1000*60*60*14); // tager kl. 14, pga. tidszoner-forskelle.
     const endDate = new Date(endYear, endMonth - 1, endDay);
     while(true) {
         if (dateToDownload.getTime() < endDate.getTime()) {
-            download24hours(dateToDownload);
+            // vi awaiter den, da vi vil tage 1 dag af gangen.
+            await download24hours(dateToDownload);
         } else {
             break;
         }
@@ -76,15 +77,25 @@ async function downloadZips(startYear, startMonth, startDay, endYear, endMonth, 
 
 async function downloadYesterday() {
     const today = new Date();
-    const oneDayInMS = 1000*60*60*24;
-    const yesterday = new Date(today.getTime() - oneDayInMS);
+    const dayInMS = 1000*60*60*24; //pga. tidszoner.
+    const yesterday = new Date(today.getTime() - dayInMS);
 
     downloadZips(yesterday.getFullYear(), yesterday.getMonth() + 1, yesterday.getDate(), today.getFullYear(), today.getMonth() + 1, today.getDate());
 }
 
-async function download24hours(date) {
-    for (let i = 1; i < 25; i++) {
-        let startdatetime = formatDate(date, i);
-        downloadZip(startdatetime);
+function download24hours(date) {
+	return new Promise((resolve, _) => {
+        for (let i = 0; i < 24; i++) {
+        // man må max kalde api'en 1 gang hvert 5. sekund. Runder op til 8 for at være sikker.
+        setTimeout(() => {
+            let startdatetime = formatDate(date, i);
+            downloadZip(startdatetime);
+        }, 8000*i);
     }
+
+    // og resolve når alle timer er downloadet, så vi kan fortsætte til næste dag:
+    setTimeout(() => {
+        resolve();
+    }, 26*8000);
+  });
 }
