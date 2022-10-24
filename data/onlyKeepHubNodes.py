5a691886f6354b21c7622e1e390e6fa5c4b913d8
Thomas Vinther <tvinther1@live.dk>
2022-10-24 12:43:42 +0200

    hubs3.csv har ingen null values

--- data/onlyKeepHubNodes.py
+++ data/onlyKeepHubNodes.py
@@ -1,10 +1,15 @@
 import pandas as pd
 
-data = pd.read_csv('../../data/dataset_newnewnewnewnewnewnewnew.csv')
+data = pd.read_csv('dataset_newnewnewnewnewnewnewnew.csv')
 first_useful_column = 'SP15_MERGED'
 index = data.columns.get_loc(first_useful_column)
 print(data.info())
 last_columns = data.iloc[:,index:]
 with_hour = data.iloc[:,0:1]
 combined = with_hour.join(last_columns)
-combined.to_csv('../../data/hubs2.csv', index=False)
+
+
+# fjern nul val række: (time 24)
+combined = combined.dropna()
+
+combined.to_csv('hubs3.csv', index=False)
