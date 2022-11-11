3c7e0521b3e3b16eb549fae1f1e805595c37c652
ThomasTVV <48794323+ThomasTVV@users.noreply.github.com>
2022-11-11 11:14:35 +0100

    Update wandb_loop.py

--- wandb_loop.py
+++ wandb_loop.py
@@ -13,8 +13,12 @@ def get_wandb_params() -> dict:
 def run(model:object):
     for season in range(4):
         dataframe:pd.DataFrame = get_data(season, wandb_params["training_length_days"]) #den her metode skal i lave
-        mae, rmse, predictions = model.train(dataframe) # klassens train() skal selv have early stopping
-        #.train() skal retunere mae, rmse, predictions for hver dag i ugen (3*7). Husk at den skal loade den bedste.
+        mae, rmse, predictions = model.train(dataframe) 
+        # train:
+        #  - skal selv have early stopping
+        #  - skal retunere mae, rmse, predictions for hver dag i ugen. (Husk at den skal loade den bedste)
+        #       - mae, rmse: (1*7).
+        #       - predictions: (36*7) 
         wandb.log([season, mae, rmse, predictions]) # check lige at det her virker
 
     wandb.done(avg_loss)
