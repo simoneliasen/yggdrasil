d2c46f421140ed26a7be8fa686165cacaada4baa
Frederik Fuglsang Midtgaard <midtgaard@hotmail.dk>
2022-12-03 02:06:39 +0100

    data exploration soon ready for report

--- data_exploration/data_exploration.py
+++ data_exploration/data_exploration.py
@@ -3,27 +3,51 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 def plot_heatmap(df):
-    df_copy = df
+    df_copy = df.copy()
     df_copy['month'] = pd.DatetimeIndex(df_copy['hour']).month
     df_copy['year'] = pd.DatetimeIndex(df_copy['hour']).year
 
     df_copy.drop(columns=['hour'],inplace=True)
+    
     df_copy_monthly = df_copy.groupby(['year','month']).mean()
+    df_copy_monthly = df_copy_monthly.transpose()
     #df_pivot = pd.pivot_table(df_cloudcover_monthly, index='YM',)
     #df_cloudcover_monthly.head()
 
-    sns.heatmap(df_copy_monthly, annot=False, cmap='RdYlBu_r', fmt= '.4g')
-    plt.show()
+    ax = sns.heatmap(df_copy_monthly, annot=False, cmap='RdYlBu_r', fmt= '.4g')
+    ax.invert_yaxis()
+    return ax
 
-df = pd.read_csv("data\\weather_data\\forecasts.csv")
-#df.columns = df.columns.str.replace("", "")
+df = pd.read_csv("data\\weather_data\\cloud_cover.csv")
+df.columns = df.columns.str.replace(" Cloud Cover Forecast", "")
 
-#plot_heatmap(df)
-columns = df.columns
-
-df_dataset  = pd.read_csv("data\\dataset_dropNa.csv")
-df_caiso_forecasts = df_dataset.loc[:,df_dataset.columns.isin(columns)]
-
+#ax = plot_heatmap(df)
+#ax.title.set_text("Cloud Cover Forecast - Monthly Average")
+#ax.collections[0].colorbar.set_label("Percentage")
 
 
-#plot_heatmap(df_caiso_forecasts)
+#df.drop(columns=['year','month'],inplace=True)
+df = df.describe().loc[['mean','std']]
+
+df = df.transpose()
+
+#ax = df.plot.scatter(x='mean',y='std',) 
+#for i, row in df.iterrows():
+#    ax.annotate(i, (row['mean'], row['std']))
+#ax.legend()
+
+df.sort_values(by=['mean'],inplace=True)
+ax = sns.scatterplot(x="mean", y="std", data=df,hue=df.index)
+ax.set_title("Mean and standard deviation of cloud cover forecast")
+ax.set_xlabel("mean")
+ax.set_ylabel("std")
+box = ax.get_position()
+ax.set_position([box.x0, box.y0 + box.height * 0.1,
+                 box.width, box.height * 0.9])
+
+# Put a legend below current axis
+ax.legend(loc='upper center', bbox_to_anchor=(0.5, -0.05),
+          fancybox=True, shadow=True, ncol=5)
+
+
+plt.show()
