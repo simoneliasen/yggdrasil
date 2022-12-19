8c777bc978fdac87e49cb9942a5b8ccd5110f20f
ThomasTVV <48794323+ThomasTVV@users.noreply.github.com>
2022-12-19 16:44:37 +0100

    Update ensamble_mae.py

--- ensamble_mae.py
+++ ensamble_mae.py
@@ -4,6 +4,7 @@ import math
 maes = []
 rmses = []
 
+# prøv at regn avg mae u fra dem her, så skal det gerne give 6,7 - ellers er der fejl.
 for file in range(12):
     csv:pd.DataFrame = pd.read_csv(fr"data/ensamble1/e{file}.csv")
     csv = csv[csv.columns.drop(list(csv.filter(regex='__MIN')))]
@@ -17,6 +18,8 @@ for file in range(12):
         avg_pred = (row[0] + row[1] + row[2] + row[3] + row[4]) / 5
         target = row[5]
         mae = abs(target - avg_pred)
+        #mae2 = abs(row[0] - target) + abs(row[1] - target) + abs(row[2] - target) + abs(row[3] - target) + abs(row[4] - target)# bare til test
+        #mae = mae2/5
         se = mae * mae
         abs_errors.append(mae)
         squared_errors.append(se)
@@ -27,6 +30,7 @@ for file in range(12):
     maes.append(mae)
     rmses.append(rmse)
 
+print(maes)
 avg_mae = sum(maes) / len(maes)
 avg_rmse = sum(rmses) / len(rmses)
 print("avg_mae", avg_mae)
